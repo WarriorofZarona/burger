8ce4d734af4a4ef17c3881238258ea012fbf230b
Andrew Bautista <andrewbautista85@gmail.com>
2020-03-19 14:16:34 -0400

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+const dotenv = require("dotenv").config();
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: process.env.PORT || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "burgers_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connection.end();
+});
+
+module.exports = connection;
---
+++ config/orm.js
@@ -0,0 +1,21 @@
+const connection = require('connection');
+
+selectAll = () => {
+
+    // connection query to select all goes here
+
+};
+
+insertOne = () => {
+
+    // connection query to insert goes here
+
+};
+
+updateOne = () => {
+
+    // connection query to update goes here
+
+};
+
+module.exports = orm;
