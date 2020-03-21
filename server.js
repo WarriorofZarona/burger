b738bc8cd71140549b4bda7da6fd0ec7c24b07d5
Andrew Bautista <andrewbautista85@gmail.com>
2020-03-20 22:09:57 -0400

    added require for express, mysql, handlebars

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const express = require('express');
-const connection = require('./config/connection');
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
 
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const connection = require('./config/connection');
