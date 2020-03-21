d6fdeb1fcbc86def778b76f745d97447a4974fa5
Andrew Bautista <andrewbautista85@gmail.com>
2020-03-20 21:16:43 -0400

    initial commit

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv").config();
 
 const connection = mysql.createConnection({
     host: "localhost",
-    port: process.env.PORT || 3306,
+    port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: "burgers_db"
