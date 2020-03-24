bd79ccd07321b6f60c448c4ebc4e40b4145ed8eb
Andrew Bautista <andrewbautista85@gmail.com>
2020-03-23 22:48:08 -0400

    added JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,24 @@
 const mysql = require("mysql");
 const dotenv = require("dotenv").config();
+let connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
 
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+    connection.connect(function (err) {
+        if (err) {
+            console.error("error connecting: " + err.stack);
+            return;
+        }
+        console.log("connected as id " + connection.threadId);
+    });
 
-module.exports = connection;
+    module.exports = connection;
